4b30ba87049716cbc4fc08f3fc3a36cd9f218aa7
Lisa <lisa.lz.zuchowski@gmail.com>
2025-03-05 16:15:34 +0100

    feat: this adds basic skeleton page code

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
 <script lang="ts">
   export default {
-    name: "Booky"
+    name: "Booky",
+    setup() {
+      definePageMeta({
+        title: "Booky · Willkommen"
+      })
+      useHead({
+        title: "Booky · Willkommen"
+      })
+    }
   }
 </script>
 
 <template>
   <div>
-    <h1>Booky</h1>
+    <h1 class="text-4xl font-bold">Booky</h1>
   </div>
 </template>
---
+++ pages/search.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  name: "Suche",
+  setup() {
+    definePageMeta({
+      title: "Booky · Suche"
+    })
+    useHead({
+      title: "Booky · Suche"
+    })
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="text-4xl font-bold">Suche</h1>
+  </div>
+</template>

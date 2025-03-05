cd3782b0460511c3feb09225b705525613a27369
Lisa <lisa.lz.zuchowski@gmail.com>
2025-03-05 16:14:49 +0100

    feat: this adds the apps layout

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Navigation from "@/components/navigation.vue"
+  export default {
+    name: 'DefaultLayout',
+    components: {
+      Navigation,
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <Navigation />
+    <main class="p-8">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  background: #333;
+  color: white;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
